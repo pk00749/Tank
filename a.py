c5bf71e3b166f51c971481dfe759ba777ba0bb1d
york_warcraft <pk00749@163.com>
2014-11-24 23:31:19 +0800

    20141124

--- a.py
+++ a.py
@@ -9,11 +9,12 @@ import math
 from pygame.locals import *
 from pygame.font import *
 
-
+CAPTION = 'Tanks'
 DIRECT_DICT = {pg.K_w : ( 0,-1),
                pg.K_s : ( 0, 1),
                pg.K_a : (-1, 0),
                pg.K_d : ( 1, 0)}
+COLOR_KEY = (0, 0, 0)
 
 def load_image(pic_name):
     #获取当前脚本文件所在目录的绝对路径
@@ -55,7 +56,8 @@ class Tank:
         self.direction_stack = []
         self.old_direction = None
         self.frames = self.get_frames()
-        self.walkframe_dict = self.make_frame_dict()
+        self.walkframe_dict = self.make_frames_dict()
+        self.bullet_frame = self.get_bullet_frame()
 
     def add_direction(self, key):
         if key in DIRECT_DICT:
@@ -73,30 +75,43 @@ class Tank:
                 self.direction = self.direction_stack[-1]
             print('Pop',self.direction_stack)
         
-    def make_frame_dict(self):
+    def make_frames_dict(self):
         frames = {  pg.K_w : self.frames[0],
                     pg.K_s : self.frames[1],
                     pg.K_a : self.frames[2],
                     pg.K_d : self.frames[3]}
         return frames
         
-    def adjust_image(self):
+    def adjust_frames(self):
         if self.old_direction != self.direction:
             self.walkframe = self.walkframe_dict[self.direction]
             self.old_direction = self.direction
                 
     def control_tank(self):
         
-        self.adjust_image()
-        tankpos = [x, y] = [0, 0]
+        self.adjust_frames()
+        TankPos = [x, y] = [0, 0]
 
         if self.direction_stack:       
             vector = DIRECT_DICT[self.direction]           
 
-            tankpos[0] += self.speed*vector[0]
-            tankpos[1] += self.speed*vector[1]
+            TankPos[0] += self.speed * vector[0]
+            TankPos[1] += self.speed * vector[1]
 
-        return tankpos
+        return TankPos
+
+    def control_bullet(self,speed):
+        self.adjust_frames()
+        BulletPos = [x, y] = [0, 0]
+
+        if self.direction_stack:
+            bullet_vector = DIRECT_DICT[self.direction]
+
+            BulletPos[0] += self.speed * bullet_vector[0]
+            BulletPos[1] += self.speed * bullet_vector[1]
+
+        return BulletPos
+        #pass
 
     def catch_event_key(self):
         for event in pg.event.get():
@@ -111,13 +126,9 @@ class Tank:
         
     
     def play_tank(self):
-        mytank_rect = mytank_up.get_rect() 
-
-        backgroud_image = load_image('backgroud.jpg')
-             
-        font = pg.font.SysFont('arial', 40)
-        text_surface = font.render('Hello', True, (0, 0, 255))
-       
+        mytank_rect = mytank_up.get_rect()
+        bullet_rect = self.bullet_frame.get_rect() 
+         
         xx = 0
         yy = 0
 
@@ -127,14 +138,17 @@ class Tank:
             self.catch_event_key()
             
             cur_speed = self.control_tank()
+            bullet_cur_speed = self.control_bullet(5)
 
             #Rect的clamp方法使用移动范围限制在窗口内
             mytank_rect = mytank_rect.move(cur_speed).clamp(window_size)
+            bullet_rect = bullet_rect.move(bullet_cur_speed).clamp(window_size)
             
             #设置窗口背景asd
             screen.blit(backgroud_image, (0, 0))
 
             screen.blit(self.walkframe, mytank_rect)
+            screen.blit(self.bullet_frame, bullet_rect)
                     
             xx -= 0.1
             if xx < -text_surface.get_width():
@@ -148,7 +162,6 @@ class Tank:
             text_pos = "Position: (%d , %d)" % (mytank_rect.left, mytank_rect.top)
             show_text(screen, (20, 450), text_pos, (0, 255, 0), False, 30, False)
             
-
             #将Surface对象上帝绘制在屏幕上		
             pg.display.flip()
 	
@@ -161,14 +174,24 @@ class Tank:
         frames.append(mytank_right)
         return frames          
 
+    def get_bullet_frame(self):
+        frames_rect = ([0, 0], (32, 32))
+        return bullet.subsurface(frames_rect)
+
+
 if __name__ == "__main__":
         pg.init()
         window_size = Rect(0, 0, 700, 500)
         screen = pg.display.set_mode(window_size.size)
-        pg.display.set_caption('Tank')
+        pg.display.set_caption(CAPTION)
+        backgroud_image = load_image('backgroud.jpg')
+        font = pg.font.SysFont('arial', 40)
+        text_surface = font.render('Hello', True, (0, 0, 255))
         mytank_up = load_image('myTank_up.png')
         mytank_down = load_image('myTank_down.png')
         mytank_left = load_image('myTank_left.png')
-        mytank_right = load_image('myTank_right.png')
-        t = Tank(5)
-        t.play_tank()
+        mytank_right = load_image('myTank_right.png')            
+        bullet = load_image('bullet_square.jpg')
+        bullet.set_colorkey(COLOR_KEY)
+        player_1 = Tank(5)
+        player_1.play_tank()
